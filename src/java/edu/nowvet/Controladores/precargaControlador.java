7c7774e78d6150298762ba03bed6346f378c67c6
Julian <Julian@DESKTOP-6M8CTLN>
2016-06-02 22:27:18 -0500

    precarga

--- src/java/edu/nowvet/Controladores/precargaControlador.java
+++ src/java/edu/nowvet/Controladores/precargaControlador.java
@@ -25,7 +25,7 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
  */
 @WebServlet(name = "precargaControlador", urlPatterns = {"/precargaControlador"})
 public class precargaControlador extends HttpServlet {
-
+public static String nombre;
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
